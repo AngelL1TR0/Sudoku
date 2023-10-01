437c4a60055b6fe47a007d98e22324be5ed574cf
Angel04 <angeltorija4@gmail.com>
2023-10-02 00:04:29 +0200

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,15 +32,15 @@ public class Main {
 
         while (true) {
             System.out.print("Ingresa la fila (1-9, 0 para terminar): ");
-            int row = scanner.nextInt();
-            if (row == 0) {
+            int fila = scanner.nextInt();
+            if (fila == 0) {
                 break;
             }
 
             System.out.print("Ingresa la columna (1-9): ");
-            int col = scanner.nextInt();
+            int columna = scanner.nextInt();
 
-            if (row < 1 || row > 9 || col < 1 || col > 9) {
+            if (!esEntradaValida(fila, columna) || !esCasillaVacia(board, fila, columna)) {
                 System.out.println("Entrada inválida. Intenta nuevamente.");
                 continue;
             }
@@ -48,20 +48,17 @@ public class Main {
             System.out.print("Ingresa el número (1-9) o '0' para borrar: ");
             int num = scanner.nextInt();
 
-            if (num < 0 || num > 9) {
+            if (!esNumeroValido(num)) {
                 System.out.println("Número inválido. Intenta nuevamente.");
                 continue;
             }
 
-            if (board[row - 1][col - 1] == 0) {
-                board[row - 1][col - 1] = num;
-            } else if (num == 0) {
-                board[row - 1][col - 1] = 0;
-            } else {
-                System.out.println("Casilla ocupada. Intenta nuevamente.");
+            if (!esMovimientoValido(board, fila, columna, num)) {
+                System.out.println("Movimiento no válido. Intenta nuevamente.");
                 continue;
             }
 
+            board[fila - 1][columna - 1] = num;
             imprimeTablero(board);
         }
 
@@ -75,6 +72,8 @@ public class Main {
                 int num = tablero[i][j];
                 if (num == 0) {
                     System.out.print("\u001B[33m" + num + "\u001B[0m "); // Amarillo para 0
+                } else if (num < 0) {
+                    System.out.print("\u001B[32m" + num + "\u001B[0m "); // Verde para números nuevos
                 } else {
                     System.out.print("\u001B[31m" + num + "\u001B[0m "); // Rojo para originales
                 }
@@ -83,4 +82,51 @@ public class Main {
         }
     }
 
+    public static boolean esEntradaValida(int fila, int columna) {
+        return fila >= 1 && fila <= 9 && columna >= 1 && columna <= 9;
+    }
+
+    public static boolean esCasillaVacia(int[][] board, int fila, int columna) {
+        return board[fila - 1][columna - 1] == 0;
+    }
+
+    public static boolean esNumeroValido(int num) {
+        return num >= 0 && num <= 9;
+    }
+
+    public static boolean esMovimientoValido(int[][] board, int fila, int columna, int num) {
+        // Verificar si el número ya existe en la misma fila, columna o cuadrante
+        return !existeEnFila(board, fila, num) && !existeEnColumna(board, columna, num) && !existeEnCuadrante(board, fila, columna, num);
+    }
+
+    public static boolean existeEnFila(int[][] board, int fila, int num) {
+        for (int col = 0; col < 9; col++) {
+            if (board[fila - 1][col] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean existeEnColumna(int[][] board, int columna, int num) {
+        for (int fila = 0; fila < 9; fila++) {
+            if (board[fila][columna - 1] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean existeEnCuadrante(int[][] board, int fila, int columna, int num) {
+        int cuadranteFilaInicio = (fila - 1) / 3 * 3;
+        int cuadranteColumnaInicio = (columna - 1) / 3 * 3;
+        for (int i = cuadranteFilaInicio; i < cuadranteFilaInicio + 3; i++) {
+            for (int j = cuadranteColumnaInicio; j < cuadranteColumnaInicio + 3; j++) {
+                if (board[i][j] == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
