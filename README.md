6e1a7657065720faadf505984f6c49ab52c90aa2
Angel04 <angeltorija4@gmail.com>
2023-10-02 23:55:46 +0200

    final commit

--- README.md
+++ README.md
@@ -2,11 +2,41 @@
 
 Este es un simple juego de Sudoku en Java, diseñado para jugar en la consola. El juego cumple con las reglas básicas del Sudoku y proporciona una interfaz de usuario simple para interactuar con el tablero.
 
+## Características
+
 - Cumple con las reglas básicas del Sudoku.
 - Interfaz de consola simple y fácil de usar.
 - Colores para destacar números editables y originales.
 - Prevención de movimientos inválidos, como números duplicados en la misma fila, columna o cuadrante.
 
-- ## Contribuir
+## Cómo Jugar
 
-Si quieres contribuir a este proyecto, siéntete libre de hacerlo. Puedes abrir problemas (issues) o enviar solicitudes de extracción (pull requests) para sugerir mejoras o correcciones.
+Para jugar al Sudoku en este programa, sigue estos pasos:
+
+1. Ejecuta la aplicación Java.
+2. Observa el tablero de Sudoku en la consola.
+3. Ingresa la fila en la que deseas colocar un número (1-9) o 0 para salir.
+4. Ingresa la columna en la que deseas colocar el número (1-9).
+5. Ingresa el número que deseas colocar (1-9) o 0 para borrar un número.
+6. Repite los pasos 3 a 5 hasta que completes el Sudoku o decidas salir.
+
+## Contribuir
+
+Si deseas contribuir a este proyecto, siéntete libre de hacerlo. Puedes abrir problemas (issues) o enviar solicitudes de extracción (pull requests) para sugerir mejoras o correcciones. Además, puedes ayudar a mejorar la documentación y los comentarios del código para hacerlo más comprensible y legible.
+Tambien hay metodos comentados ya que fueron versiones anteriores, toda mejora es bienvenida
+
+## Métodos JavaDoc
+
+Hemos agregado comentarios de JavaDoc a los métodos para mejorar la comprensión del código. Aquí tienes una descripción breve de los nuevos métodos:
+
+- `verificarSudoku(int[][] tablero)`: Verifica si el tablero de Sudoku es válido, comprobando filas, columnas y cuadrantes.
+- `verificarCuadrante(int[][] tablero)`: Verifica si un número en una posición específica de un cuadrante ya existe en el mismo.
+- `verificarColumna(int[][] tablero)`: Verifica que no se repitan números en la misma columna.
+- `verificarFila(int[][] tablero)`: Verifica que no se repitan números en la misma fila.
+- `comprobar(int[][] tablero)`: Inicia el juego de Sudoku y permite al usuario ingresar números en el tablero.
+- `imprimirTablero(int[][] tablero)`: Imprime el tablero de Sudoku en la consola.
+- `esCasillaEditable(int[][] tablero, int fila, int columna)`: Verifica si una casilla en el tablero de Sudoku es editable.
+- `esMovimientoValido(int[][] tablero, int fila, int columna, int numero)`: Verifica si un movimiento (inserción de un número) en el tablero de Sudoku es válido.
+- `existeEnFila(int[][] tablero, int fila, int numero)`: Verifica si un número ya existe en la misma fila en el tablero de Sudoku.
+- `existeEnColumna(int[][] tablero, int columna, int numero)`: Verifica si un número ya existe en la misma columna en el tablero de Sudoku.
+- `existeEnCuadrante(int[][] tablero, int fila, int columna, int numero)`: Verifica si un número ya existe en el mismo cuadrante 3x3 en el tablero de Sudoku.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         int[][] tablero = {
+
+                //Tablero para jugar
+                /*
                 {5, 3, 0, 0, 7, 0, 0, 0, 0},
                 {6, 0, 0, 1, 9, 5, 0, 0, 0},
                 {0, 9, 8, 0, 0, 0, 0, 6, 0},
@@ -15,17 +19,195 @@ public class Main {
                 {0, 6, 0, 0, 0, 0, 2, 8, 0},
                 {0, 0, 0, 4, 1, 9, 0, 0, 5},
                 {0, 0, 0, 0, 8, 0, 0, 7, 9}
+                 */
+
+                //tablero bien
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+
+
+                //tablero mal
+                /*
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 5}
+                 */
+
         };
 
-        jugar(tablero);
+        //Metodo para empezar a jugar
+        //comprobar(tablero);
+
+        //Condicion que determina si la plantilla esta bien o no
+        if (verificarSudoku(tablero)) {
+            System.out.println("El sudoku está resuelto correctamente.");
+        } else {
+            System.out.println("El sudoku no está resuelto correctamente.");
+        }
     }
 
     /**
-     * Selecciona la fila y columna a jugar para introducir un numero
-     * Despues detecta que se pueda jugar en esa casilla y si se cumplen las normas del sudoku
-     * @param tablero
+     * Comprueba si un tablero de Sudoku es válido o no.
+     *
+     * @param tablero El tablero de Sudoku a comprobar.
+     * @return `true` si el Sudoku es válido, `false` si no lo es.
      */
-    public static void jugar(int[][] tablero) {
+    private static boolean verificarSudoku(int[][] tablero) {
+        // Verificaciones
+        if (verificarFila(tablero) || verificarColumna(tablero)
+                || verificarCuadrante(tablero)) return false;
+
+        return true;
+    }
+
+    /**
+     * Verifica si un número en una posición específica de un cuadrante ya existe en el mismo.
+     *
+     * @param tablero El tablero de Sudoku a comprobar.
+     * @return `true` si se encuentra un número repetido en un cuadrante, `false` si no se encuentra.
+     */
+    public static boolean verificarCuadrante(int[][] tablero) {
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                ArrayList<Integer> numerosVistos = new ArrayList<>();
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        int numero = tablero[x][y];
+                        if (numerosVistos.contains(numero)) {
+                            return true;
+                        }
+                        numerosVistos.add(numero);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+        /*
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                for (int pos = 0; pos < 8; pos++) {
+                    for (int pos2 = pos + 1; pos2 < 9; pos2++) {
+                        int x1 = i + pos % 3;
+                        int y1 = j + pos / 3;
+                        int x2 = i + pos2 % 3;
+                        int y2 = j + pos2 / 3;
+
+                        if (tablero[x1][y1] == tablero[x2][y2]) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+        int cuadranteFilaInicio = (i / 3) * 3;
+        int cuadranteColumnaInicio = (j / 3) * 3;
+        for (int x = cuadranteFilaInicio; x < cuadranteFilaInicio + 3; x++) {
+            for (int y = cuadranteColumnaInicio; y < cuadranteColumnaInicio + 3; y++) {
+                int num = tablero[x][y];
+                for (int w = cuadranteFilaInicio; w < cuadranteFilaInicio + 3; x++) {
+                    for (int z = w + 1; z < cuadranteColumnaInicio + 3; y++) {
+                        if (x != w && y != z && tablero[x][y] == num) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+         */
+
+    /**
+     * Verifica que no se repitan números en la misma columna.
+     *
+     * @param tablero El tablero de Sudoku a comprobar.
+     * @return `true` si se encuentra un número repetido en una columna, `false` si no se encuentra.
+     */
+    public static boolean verificarColumna(int[][] tablero) {
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                for (int k = i + 1; k < 9; k++) {
+                    if (tablero[i][j] == tablero[k][j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    /*
+        for (int j = 0; j < 9; j++) {
+            ArrayList<Integer> repetidos = new ArrayList<>();
+            for (int i = 0; i < 9; i++) {
+                int numero = tablero[i][j];
+                if (repetidos.contains(numero)) {
+                    return true;
+                }
+                repetidos.add(numero);
+            }
+        }
+        return false;
+    }
+
+     */
+
+    /**
+     * Verifica que no se repitan números en la misma fila.
+     *
+     * @param tablero El tablero de Sudoku a comprobar.
+     * @return `true` si se encuentra un número repetido en una fila, `false` si no se encuentra.
+     */
+    public static boolean verificarFila(int[][] tablero) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                for (int k = j + 1; k < 9; k++) {
+                    if (tablero[i][j] == tablero[i][k]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+        /*
+        for (int i = 0; i < 9; i++) {
+            ArrayList<Integer> repetidos = new ArrayList<>();
+            for (int j = 0; j < 9; j++) {
+                int numero = tablero[i][j];
+                if (repetidos.contains(numero)) {
+                    return true;
+                }
+                repetidos.add(numero);
+            }
+        }
+        return false;
+    }
+         */
+
+
+    /**
+     * Inicia el juego de Sudoku, permitiendo al usuario ingresar números en el tablero.
+     *
+     * @param tablero El tablero de Sudoku en el que se jugará.
+     */
+    public static void comprobar(int[][] tablero) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -41,7 +223,6 @@ public class Main {
 
             if (!esCasillaEditable(tablero, fila, columna)) {
                 System.out.println("No puedes modificar este número.");
-                continue;
             }
 
             System.out.println("Ingresa el número (1-9) o '0' para borrar: ");
@@ -54,12 +235,12 @@ public class Main {
             }
         }
 
-        scanner.close();
     }
 
     /**
-     * Imprime el tablero de juego
-     * @param tablero
+     * Imprime el tablero de Sudoku en la consola.
+     *
+     * @param tablero El tablero de Sudoku a imprimir.
      */
     public static void imprimirTablero(int[][] tablero) {
         System.out.println("Tablero de Sudoku:");
@@ -78,23 +259,25 @@ public class Main {
     }
 
     /**
-     * Detecta si se puede jugar en esa casilla, es decir que sea un '0'
-     * @param tablero
-     * @param fila
-     * @param columna
-     * @return
+     * Verifica si una casilla en el tablero de Sudoku es editable (es decir, no contiene un número fijo).
+     *
+     * @param tablero El tablero de Sudoku.
+     * @param fila    La fila de la casilla.
+     * @param columna La columna de la casilla.
+     * @return `true` si la casilla es editable, `false` si no lo es.
      */
     public static boolean esCasillaEditable(int[][] tablero, int fila, int columna) {
         return tablero[fila - 1][columna - 1] == 0;
     }
 
     /**
-     * Llama al resto de metodos para comprobar que se cumplan las reglas del sudoku
-     * @param tablero
-     * @param fila
-     * @param columna
-     * @param numero
-     * @return
+     * Verifica si un movimiento (inserción de un número) en el tablero de Sudoku es válido.
+     *
+     * @param tablero El tablero de Sudoku.
+     * @param fila    La fila en la que se realiza el movimiento.
+     * @param columna La columna en la que se realiza el movimiento.
+     * @param numero  El número que se intenta colocar en la casilla.
+     * @return `true` si el movimiento es válido, `false` si no lo es.
      */
     public static boolean esMovimientoValido(int[][] tablero, int fila, int columna, int numero) {
         return !existeEnFila(tablero, fila, numero) &&
@@ -103,11 +286,12 @@ public class Main {
     }
 
     /**
-     * Detecta que el mismo numero no este en la misma fila
-     * @param tablero
-     * @param fila
-     * @param numero
-     * @return
+     * Verifica si un número ya existe en la misma fila en el tablero de Sudoku.
+     *
+     * @param tablero El tablero de Sudoku.
+     * @param fila    La fila en la que se verifica.
+     * @param numero  El número a verificar.
+     * @return `true` si el número ya existe en la fila, `false` si no existe.
      */
     public static boolean existeEnFila(int[][] tablero, int fila, int numero) {
         for (int i = 0; i < 9; i++) {
@@ -119,11 +303,12 @@ public class Main {
     }
 
     /**
-     * Detecta que el mismo numero no este en la misma columna
-     * @param tablero
-     * @param columna
-     * @param numero
-     * @return
+     * Verifica si un número ya existe en la misma columna en el tablero de Sudoku.
+     *
+     * @param tablero El tablero de Sudoku.
+     * @param columna La columna en la que se verifica.
+     * @param numero  El número a verificar.
+     * @return `true` si el número ya existe en la columna, `false` si no existe.
      */
     public static boolean existeEnColumna(int[][] tablero, int columna, int numero) {
         for (int i = 0; i < 9; i++) {
@@ -135,12 +320,13 @@ public class Main {
     }
 
     /**
-     * Detecta que el mismo numero introducido no este en el mismo cuadrante 3x3
-     * @param tablero
-     * @param fila
-     * @param columna
-     * @param numero
-     * @return
+     * Verifica si un número ya existe en el mismo cuadrante 3x3 en el tablero de Sudoku.
+     *
+     * @param tablero El tablero de Sudoku.
+     * @param fila    La fila de la casilla.
+     * @param columna La columna de la casilla.
+     * @param numero  El número a verificar.
+     * @return `true` si el número ya existe en el cuadrante, `false` si no existe.
      */
     public static boolean existeEnCuadrante(int[][] tablero, int fila, int columna, int numero) {
         int cuadranteFilaInicio = (fila - 1) / 3 * 3;
